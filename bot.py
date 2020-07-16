e93a9a6ab7f13cac3f885e6b9addf07c24843d06
Brandon Gallagher <brandonegallagher@gmail.com>
2020-07-16 01:45:36 -0400

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+from modules.pirate_api import Pirate_API
+from discord import Embed
+import datetime
+server = commands.Bot(command_prefix="!!")
+
+pirateAPI = Pirate_API()
+
+# @server.event
+# async def on_ready():
+#     print("okay")
+
+token = ""
+with open("config.txt", "r") as f:
+    token = str(f.readlines()[0])
+
+@server.command()
+async def torrent(ctx, *args) -> str:
+    
+    if (len(args) < 1):
+        await ctx.send("Please provide the name of the torrent (movie/game/etc)")
+    else:
+        try:
+
+            query: str = ""
+            for string in args:
+                query += " " + string
+            print(query)
+
+            torrent: dict = pirateAPI.get_torrent(query)[0]
+
+            embed = discord.Embed()
+            embed.set_image(url=torrent["image_url"])
+            embed.set_author(name=args[0])
+            embed.set_footer(text=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            embed.add_field(name="Name", value=torrent["name"], inline=False)
+            embed.add_field(name="Magnet", value=torrent["magnet"], inline=False)
+
+            await ctx.send(embed=embed)
+        except:
+            await ctx.send("Torrent not found, please try another query.")
+
+
+server.run(token)
---
+++ modules/pirate_api.py
@@ -0,0 +1,82 @@
+import sys
+sys.path.append(".")
+
+from tpb import TPB
+from tpb import ORDERS
+from typing import List
+from modules.image_api import ImageAPI
+
+
+class Pirate_API():
+    '''
+    This class allows my endpoints to interact with the database as well as retrieve
+    information from ThePirateBay's database.
+    '''
+    
+    def __init__(self):
+
+        self.iAPI = ImageAPI()
+        self.website = TPB("https://thepiratebay.monster/") #Base URL for ThePirateBay
+
+    def get_none(self) -> List[dict]:
+        '''
+        Returns an empty torrent object
+
+        @returns -> None
+        '''
+
+        return [{
+            "name" : "",
+            "magnet" : "",
+            "image" : ""
+        }]
+
+    def get_torrent(self, req: str) -> dict:
+        '''
+        Returns top torrent sorted by seeders
+        @req {str} -> The name of the torrent (movie, game, etc)
+        '''
+        torrent = self.website.search(req).order(ORDERS.SEEDERS.DES)
+        counter: int = 0
+        obj: List[dict]= []
+        
+        #This list can't be substriptable.. Old python problems
+        for torrent in self.website.search(req).order(ORDERS.SEEDERS.DES):
+            if counter <= 0:
+                obj.append(
+                    {
+                        "name" : torrent.title,
+                        "magnet" : torrent.magnet_link,
+                        "image_url" : self.iAPI.get_image(req),
+                    }
+                )
+            counter += 1
+        return obj
+
+    def get_torrents(self, req: str) -> List[dict]:
+        '''
+        Gets all torrents relative to the search query.
+
+        @param {str} req: The search query to be made to ThePirateBay
+        @returns {List[dict]} 3 top torrent results sorted by seeders.
+        '''
+
+        counter: int = 0
+        obj: List[dict]= []
+
+        button_id: str = "pirate_button_"
+        button_count: int = 1
+
+        for torrent in self.website.search(req).order(ORDERS.SEEDERS.DES):
+            if counter <= 2:
+                obj.append(
+                    {
+                        "name" : torrent.title,
+                        "magnet" : torrent.magnet_link,
+                        "image_url" : self.iAPI.get_image(req),
+                        "button_id" : button_id + str(button_count),
+                    }
+                )
+            counter += 1
+            button_count += 1
+        return obj
